5cd600cd44234aaba257d5b6280ba762d1b859bd
qqequalstears <christoffer.salomonsson@hotmail.com>
2024-04-17 09:22:04 +0200

    Skrivit kod för logga in och registrera fönster

--- Project/src/controller/UserGUIController.java
+++ Project/src/controller/UserGUIController.java
@@ -1,4 +1,83 @@
 package controller;
 
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import view.userStage.*;
+
 public class UserGUIController {
+    private UserEastPanel userEastPanel;
+    private UserWestPanel userWestPanel;
+    private UserSouthPanel userSouthPanel;
+    private UserMainScene userMainScene;
+    private RegisterNorthPanel registerNorthPanel;
+    private RegisterWestPanel registerWestPanel;
+    private RegisterSouthPanel registerSouthPanel;
+    private RegisterMainScene registerMainScene;
+    public UserGUIController(){
+        userMainScene = new UserMainScene(this);
+    }
+    public void createUserWindow(){
+        userMainScene.createUserWindow();
+    }
+    public void createRegisterWindow(){
+        registerMainScene = new RegisterMainScene(this);
+        registerMainScene.createRegisterWindow();
+    }
+    public TextField getLoginUserNameTextField(){
+        if(userWestPanel.getUserNameTextField() != null) {
+            TextField userName = userWestPanel.getUserNameTextField();
+            String stringUserName = userName.getText().trim();
+            if(!stringUserName.isEmpty()) {
+                return userName;
+            }
+        }
+            return null;
+    }
+    public PasswordField getLoginPasswordField(){
+        if(userWestPanel.getPasswordTextField() != null){
+            PasswordField password = userWestPanel.getPasswordTextField();
+            String passwordField = password.getText().trim();
+            if(!passwordField.isEmpty()){
+                return password;
+            }
+        }
+        return null;
+    }
+    public TextField getRegisterUsername(){
+        if(registerWestPanel.getRegisterUserName() != null){
+            TextField userName = registerWestPanel.getRegisterUserName();
+            String stringUserName = userName.getText().trim();
+            if(!stringUserName.isEmpty()){
+                return userName;
+            }
+        }
+        return null;
+    }
+    public PasswordField getRegisterFirstPassword(){
+        if(registerWestPanel.getRegisterFirstPassword() != null){
+            PasswordField password = registerWestPanel.getRegisterFirstPassword();
+            String passwordField = password.getText().trim();
+            if(!passwordField.isEmpty()){
+                return password;
+            }
+        }
+        return null;
+    }
+    public TextField getRegisterSecondPassword(){
+        if(registerWestPanel.getRegisterSecondPassword() != null){
+            PasswordField password = registerWestPanel.getRegisterSecondPassword();
+            String passwordField = password.getText().trim();
+            if(!passwordField.isEmpty()){
+                return password;
+            }
+        }
+        return null;
+    }
+
+    public void setUserWestPanel(UserWestPanel userWestPanel) {
+        this.userWestPanel = userWestPanel;
+    }
+    public void setRegisterWestPanel(RegisterWestPanel registerWestPanel){
+        this.registerWestPanel = registerWestPanel;
+    }
 }
---
+++ Project/src/view/userStage/RegisterNorthPanel.java
@@ -0,0 +1,11 @@
+package view.userStage;
+
+import controller.UserGUIController;
+import model.GetGUIController;
+
+public class RegisterNorthPanel {
+    private UserGUIController userGUIController;
+    public RegisterNorthPanel(){
+        this.userGUIController = GetGUIController.getUserGUIController();
+    }
+}
